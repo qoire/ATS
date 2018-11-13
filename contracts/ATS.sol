6933c4c3813f62260e66c4b6d13b848fc690d6e3
qoire <mr.sun.yao@gmail.com>
2018-11-12 21:27:18 -0500

    add licensing information
    update to clarify
    
    use txt format

--- contracts/ATS.sol
+++ contracts/ATS.sol
@@ -1,3 +1,15 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This code has not been reviewed.
+ * Do not use or deploy this code before reviewing it personally first.
+ *
+ * Contributors:
+ * https://github.com/jacquesd
+ * https://github.com/qoire
+ */
+
 pragma solidity 0.4.15;
 
 interface ATS {
